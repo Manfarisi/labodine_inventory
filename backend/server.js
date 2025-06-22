70afa7b4336875435a213039a4827d5bebad4e13
Salman <kingmansalman@gmail.com>
2025-06-22 09:59:22 +0700

    cors baru

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,9 @@ import absenRouter from './routes/absenRoute.js'
 // app config
 const app = express()
 const port = process.env.PORT || 4000
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 
 
